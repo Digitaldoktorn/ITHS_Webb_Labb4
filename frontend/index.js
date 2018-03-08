38b5b9b3a6666e2b6c5b601cddad62c5af1bd36f
Tom kamande <toklama40@gmail.com>
2018-03-08 14:44:40 +0100

    discard

--- frontend/index.js
+++
@@ -1,9 +0,0 @@
-
-var React = require('react');
-var ReactDOM = require('react-dom');
-var ChatApp = require('./chat-app-component.jsx');
-
-ReactDOM.render(
-    React.createElement(ChatApp),
-    document.getElementById('app')
-);
