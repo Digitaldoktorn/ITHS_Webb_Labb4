28d5fb7f37ef59177bef7bcb140e7ee7eff3f4da
Izabela Walczak <iza.niznik@gmail.com>
2018-04-05 12:09:37 +0200

    b

--- frontend/chat-app-component.jsx
+++ frontend/chat-app-component.jsx
@@ -1,61 +1,12 @@
 var React = require('react');
 
 require('./style.css');
-//require('./fonts/candy-inc/billy-argel_candy-inc/CANDY___.ttf');
-//var logo = require('./images/logo.png');
 
 class ChatAppComponent extends React.Component {
 
     constructor() {
         super();
         this.state = {
-            user: '',
-            msg: '',
-            friend: '',
-            allMessages: [],
-            loginScreen: 'show',
-            login: 'login',
-            register: 'register',
-            users: [ 	{
-	'_id': '5ab63f64d26c7b053ed879df',
-	'name': 'user1',
-	'password': 'abcd',
-	'checkout': 'true',
-	'friends': [
-		{
-			'friendsname': 'user2',
-			'status': 'confirmed'
-		},
-		{
-			'friendsname': 'user3',
-			'status': 'pending'
-		},
-		{
-			'friendsname': 'edvin',
-			'status': 'pending'
-		},
-		{
-			'friendsname': 'edvin',
-			'status': 'pending'
-		},
-		{
-			'friendsname': 'edvin',
-			'status': 'pending'
-		}
-	]
-}]
-        };
-        this.usernameChangeHandler = this.usernameChangeHandler.bind(this);
-        this.passwordChangeHandler = this.passwordChangeHandler.bind(this);
-        this.passwordConfChangeHandler = this.usernameSubmitHandler.bind(this);
-        this.emailChangeHandler = this.emailChangeHandler.bind(this);
-        this.emailConfChangeHandler = this.emailConfChangeHandler.bind(this);
-        this.usernameSubmitHandler = this.usernameSubmitHandler.bind(this);
-        this.submitLogin = this.submitLogin.bind(this);
-        this.submitSignUp = this.submitSignUp.bind(this);
-        this.saveMsg = this.saveMsg.bind(this);
-        this.sendMsg = this.sendMsg.bind(this);
-
             user: undefined,
             rec: undefined,
             string: '',
@@ -117,7 +68,6 @@ class ChatAppComponent extends React.Component {
                 method: 'PUT'
             });
         }
-
     }
 
 
@@ -227,12 +177,13 @@ class ChatAppComponent extends React.Component {
 
             var myFriends = this.state.allUsers.filter(function(me){
                 return me.name === this.state.user;
-            }.bind(this)).filter(function(friends){
-                if (friends.friends){
-                    return friends.friends;
+            }.bind(this)).filter(function(current){
+                console.log(current, this.state.allUsers);
+                if (current.friends){
+                    return current.friends;
                 }
                 return;
-            });
+            }.bind(this));
 
             if (myFriends.length > 0){
 
@@ -325,51 +276,6 @@ class ChatAppComponent extends React.Component {
           });
         }
 
-<<<<<<< HEAD
-    usernameSubmitHandler(event) {
-          event.preventDefault();
-            this.setState({ submitted: true, username: this.state.username });
-
-          }
-
-        /*fetch('/user/' + user.name, {
-            body: '{"name": "' + this.state.user + '", "status": "pending"}',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            method: 'PUT'
-        });*/
-
-        /*fetch('/user').then(function (response) {
-            return response.json();
-        }).then(function (result) {
-        console.log(result);
-        response.send({"name": result} === {this.state.username} && {"password": result} === {this.state.password}  );
-    }*/
-
-    register(){
-        this.setState({user: this.state.username,
-                      password: this.state.password }, function(){
-            var occupied = this.state.allUsers.filter(function(user){
-                return user.name == this.state.username;
-            }.bind(this));
-
-            this.state.username !== undefined && occupied.length > 0 ? this.setState({error: <h2>Sorry, username is already taken</h2>}) : this.state.password  !== this.state.passwordConf ? this.setState({error: <h2>Passwords does not match</h2>}) :
-                fetch('/user', {
-                    body: '{"name":"' + this.state.user + '", "password":"' + this.state.password + '"}',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    method: 'POST'
-                }).then( this.setState({login: 'off'}));
-});
-}
-
-
-friendRequest(user){
-  console.log(user);
-}
-
             fetch('/user').then(function (response) {
                 return response.json();
             }).then(function (result) {
@@ -393,7 +299,6 @@ friendRequest(user){
 
     }
 
-
     componentWillUnmount() {
         this.lastCheck();
         window.removeEventListener('beforeunload', this.lastCheck);
@@ -402,73 +307,6 @@ friendRequest(user){
 
     render() {
         return <div>
-
-                  <div id={this.state.login}>
-                    <div><img src="logo.png"/></div>//does not work
-                      <div id="welcome_msg">
-                   <h1>Welcome to ChatApp!</h1>
-                   <h2>The chat for you!</h2>
-                   </div>
-                    <div className="start_container">
-                      <form>
-                        <h1>Log in</h1>
-                        <div className="login">
-                          <input className= "input_start" type="text" onChange={this.usernameChangeHandler} placeholder="Username" required />
-                        </div>
-                        <div>
-                          <input className= "input_start" type="password" onChange={this.passwordChangeHandler} placeholder="Password" required /></div>
-                          <button className= "button_start" type="submit" onClick={function(){this.setState({login: 'none'}, this.submitLogin);}.bind(this)}>Log in</button>
-                        </form>
-                        <p>Don't have an account?</p>
-                        <button className="button_start" type="submit" onClick={function(){this.setState({register: 'block', login: 'none'});}.bind(this)}>Sign up here</button>
-                      </div>
-                    </div>
-
-        <div id={this.state.register}>
-            <form className="start_container">
-                <h1>Register</h1>
-                <div>
-                    <input type="text" onChange={this.usernameSubmitHandler} placeholder="Username" required /> </div>
-                <div>
-                    <input type="email" onChange={this.emailChangeHandler} placeholder="Email" required /> </div>
-                <div>
-                    <input type="email" onChange={this.emailConfChangeHandler} placeholder="Confirm your email" required /> </div>
-                <div>
-                    <input type="password" onChange={this.passwordChangeHandler} placeholder="Password" required /> </div>
-                <div>
-                    <input type="password" onChange={this.passwordConfChangeHandler} placeholder="Confirm your password" required /> </div>
-                <button className="button_start" type="submit" onClick={function(){this.setState({register: 'none'}, this.submitSignUp);}.bind(this)}>Register</button></form>
-        </div>
-
-        <div id="window">
-
-            <div>
-              <div id="profile">
-                <div id="profile_box">
-                  <div><img id="profile_picture" src="user.jpg"/></div>
-                  <p>John Snow</p><p>Arizona, USA</p></div>
-                  <div id="profile_list">
-                    <h3>Friends</h3>
-                    <h3>Notification</h3>
-                    <h3>Messages</h3>
-                    <h3>Pictures</h3>
-                    <h3>Friends</h3>
-                    <h3>Groups</h3>
-                    <h3>Settings</h3>
-                  </div>
-              </div>
-                <h1>ChatApp!</h1>
-                <div className="login-pop">
-
-                    <form onSubmit={this.usernameSubmitHandler} className="username-container">
-                        <h1>React Instant Chat</h1>
-                        <div>
-                            <input type="text" onChange={this.usernameChangeHandler} placeholder="Enter a username..." required />
-                        </div>
-                        <input type="submit" value="Submit" />
-                    </form>
-                </div>
-
           <div class={this.state.login}>
                 <div class="sign-in">
 
@@ -576,10 +414,10 @@ friendRequest(user){
               </div>
             </div>
 
-
             <div class="main">
 
-            {this.state.user == undefined || this.state.rec == undefined ? <div class="field"><h1>welcome to ChatApp!</h1></div> : <div><div class='field'>
+                {this.state.user == undefined || this.state.rec == undefined ? <div class="field"><h1>Welcome to ChatApp!</h1></div> : <div><div class='field'>
+
                 {this.state.messages.map(function(msg) {
                     var marker = msg.from === this.state.user ? 'send': 'rec';
                     var sender = msg.from === this.state.user ? 'you': msg.from;
